71919cf1e36e2fd74fe249fbd01f14d183698771
Urja Sheth <urja@devrepublic.nl>
2020-12-10 12:08:53 +0530

    conditionally republish video on resume

--- android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
+++ android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
@@ -175,6 +175,7 @@ public class CustomTwilioVideoView extends View implements LifecycleEventListene
      */
     private static PatchedVideoView thumbnailVideoView;
     private static LocalVideoTrack localVideoTrack;
+    private boolean isLocalVideoPublished = false;
 
     private static CameraCapturer cameraCapturer;
     private LocalAudioTrack localAudioTrack;
@@ -313,7 +314,7 @@ public class CustomTwilioVideoView extends View implements LifecycleEventListene
                 /*
                  * If connected to a Room then share the local video track.
                  */
-                if (localParticipant != null) {
+                if (localParticipant != null && isLocalVideoPublished) {
                     localParticipant.publishTrack(localVideoTrack);
                 }
             }
@@ -610,6 +611,7 @@ public class CustomTwilioVideoView extends View implements LifecycleEventListene
 
     public void publishLocalVideo(boolean enabled) {
         if (localParticipant != null && localVideoTrack != null) {
+            isLocalVideoPublished = enabled;
             if (enabled) {
                 localParticipant.publishTrack(localVideoTrack);
             } else {
